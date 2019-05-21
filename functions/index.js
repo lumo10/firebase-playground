6725c5282401b63c534f32d22b2984d757a25daa
Lucio <luciomorelli10@gmail.com>
2019-05-21 23:04:41 +0200

    init api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors')({origin: true});
+app.use(cors);
+
+app.get('/getLinks', async (req, res) => {
+
+  let links = ['a','b','c'];
+
+  return res.json(links);
+});
+
+app.post('/setLinks', async (req, res) => {
+  return res.send("wip");
+});
+
+exports.app = functions.https.onRequest(app);
